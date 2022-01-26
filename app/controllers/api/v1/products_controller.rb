39c9d6b565a0747366488f50c0fb688e1003f2a9
MatiasDevJam <matias.devjam@gmail.com>
2022-01-26 19:03:09 -0300

    Finishes modeling the product model along with user associations

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+    before_action :check_login, only: %i[create]
 
     def index
         render json: Product.all
@@ -8,4 +9,19 @@ class Api::V1::ProductsController < ApplicationController
         render json: Product.find(params[:id])
     end
     
+    def create
+        product = current_user.products.build(product_params)
+        
+        if product.save
+            render json: product, status: :created
+        else
+            render json: { errors: product.errors }, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def product_params
+    params.require(:product).permit(:title, :price, :published)
+    end
+
 end
--- test/controllers/api/v1/products_controller_test.rb
+++ test/controllers/api/v1/products_controller_test.rb
@@ -14,10 +14,30 @@ class Api::V1::ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should show product" do
     get api_v1_product_url(@product), as: :json
     assert_response :success
-    
+
     json_response = JSON.parse(self.response.body)
     assert_equal @product.title, json_response['title']
   end
+
+  test 'should create product' do
+    assert_difference('Product.count') do
+      post api_v1_products_url,
+      params: { product: { title: @product.title, price:@product.price, published: @product.published } },
+      headers: { Authorization: JsonWebToken.encode(user_id: @product.user_id) },
+      as: :json
+    end
+      assert_response :created
+    end
+
+  test 'should forbid create product' do
+    assert_no_difference('Product.count') do
+      post api_v1_products_url,
+      params: { product: { title: @product.title, price:@product.price, published: @product.published } },
+      as: :json
+    end
+      assert_response :forbidden
+    end
+  
   
   
 end
