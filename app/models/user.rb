1ff35f6bbb20fb66b80b9723dc5d630e46ed7662
MatiasDevJam <matias.devjam@gmail.com>
2022-01-26 16:15:16 -0300

    Setup Bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+    # bcrypt
+    has_secure_password
+
+    # validates user
     validates :email, uniqueness: true
     validates_format_of :email, with: /@/
     validates :password_digest, presence: true
