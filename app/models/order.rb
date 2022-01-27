c4ade141d6f11e5fb1e64b994f93064f4589d91f
MatiasDevJam <matias.devjam@gmail.com>
2022-01-27 16:07:12 -0300

    Adds validator for order with not enough products on stock

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
+  
+  validates_with EnoughProductsValidator
+
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "an order should command not too much product than available" do
+    @order.placements << Placement.new(product_id: @product1.id, quantity: (1 + @product1.quantity))
+    assert_not @order.valid?
+  end
+  
 end
