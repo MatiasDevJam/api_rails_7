b2dd92c25a2c8ea20b26b54c57c80b7ded790ab0
MatiasDevJam <matias.devjam@gmail.com>
2022-01-27 16:48:23 -0300

    Factorize pagination

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -1,13 +1,18 @@
 module Paginable
     protected
 
-    def currente_page
-        (params[:page] || 1).to_i
-    end
+    def get_links_serializer_options links_paths, collection 
+        {
 
-    def per_page
-        (params[:per_page] || 20).to_i
+            links: {
+                first: send(links_paths, page: 1),
+                last: send(links_paths, page: collection.total_pages),
+                prev: send(links_paths, page: collection.prev_page),
+                next: send(links_paths, page: collection.next_page),
+            }
+        }
     end
+      
     
     
 end
