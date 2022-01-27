aad1bff5043f5565d4f95297f07fd3b32bf47e07
MatiasDevJam <matias.devjam@gmail.com>
2022-01-27 16:11:34 -0300

    Updates the total calculation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,9 @@ class Order < ApplicationRecord
   
   validates_with EnoughProductsValidator
 
+  def set_total!
+    self.total = self.placements.map{ |placement| placement.product.price * placement.quantity }.sum
+  end
+  
+
 end
